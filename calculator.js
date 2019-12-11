608fb2fd5d6b33ade3c80af5daef92441cfb7207
xolani <xolani.mgube@umuzi.org>
2019-12-11 16:08:31 +0200

    almost done with no. 4

--- calculator.js
+++ calculator.js
@@ -1,28 +1,35 @@
 function add(string){
     var sum = 0;
-
-    //var newDeli = /(?<=\/\/).*?(?=\n)/;
-
-   //var newDeliValue = string.match(newDeli);
-
-  
-
-    var four = 4;
-
+    
     var delimiters = /,|\n|(?<=\/\/).*?(?=\n)|\/|1000*|\W|[a-zA-Z]|/;
+
+
+    if (string.includes("//") == true){
+        var beforeNewline = string.match(/(?<=\/\/).*?(?=\n)/g)[0]
+        console.log(beforeNewline + " this is the delimiter")
+     
+        var afterNewline = string.match(/(?<=\n).*/g)[0]
+        console.log(afterNewline, " this is the after new line")
+
+        // var stringSplit = afterNewline.replace(beforeNewline, ",").split(",")
+        // console.log(stringSplit)
+
+        afterNewline = afterNewline.split(beforeNewline);
+        console.log(afterNewline, "newline")
+
+        //string = string.split()
+        
+    }
 
     var invalidString;
 
     var negativeInt = /-\d+?/g //use g to output every match thano typet is found
-    no type
-
     
-
     if (string.match(negativeInt)){
-        return "Negative numbers are not allowed " + string.match(negativeInt);
+        //return "Negative numbers are not allowed " + string.match(negativeInt);
+        throw new Error("negative numbers are not allowed")
     }
 
-    
     
     string = string.split(delimiters);
 
@@ -40,8 +47,9 @@ function add(string){
 
     return sum;
 }
-console.log(add("//;\n1000;142;"))
- //console.log(add("-2"))
+console.log(add("//a\n1a2"))
+console.log(add("1,2,3,4"))
+console.log(add("-2"))
 
 
 
